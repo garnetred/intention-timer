c1204e3cbb88db9246b9eb0206ecc1b388a82092
December G <dgarnetsmith@gmail.com>
2020-02-06 15:40:59 -0700

    Began iteration 1.2

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ var secondsInput = document.getElementById("seconds-input");
 var studyButton = document.getElementById("study");
 var meditateButton = document.getElementById("meditate");
 var exerciseButton = document.getElementById("exercise");
+var studyImg = document.querySelector(".study-img");
+var meditateImg = document.querySelector(".meditate-img");
+var exerciseImg = document.querySelector(".exercise-img");
 
 minutesInput.addEventListener('keyup', onlyNumbersCheck);
 secondsInput.addEventListener('keyup', onlyNumbersCheck);
-studyButton.addEventListener('click', changeColors)
-meditateButton.addEventListener('click', changeColors)
-exerciseButton.addEventListener('click', changeColors)
+studyButton.addEventListener('click', changeStudyBox);
+meditateButton.addEventListener('click', changeMeditateBox);
+exerciseButton.addEventListener('click', changeExerciseBox);
+// window.addEventListener('load', inactiveButtons);
 
 function onlyNumbersCheck() {
   var validChars = "0123456789.";
@@ -21,7 +25,59 @@ function onlyNumbersCheck() {
   }
 }
 
-function changeColors() {
-  
+// function inactiveButtons() {
+//   studyImg.classList.toggle("hidden");
+//   meditateImg.classList.toggle("hidden");
+//   exerciseImg.classList.toggle("hidden");
+// }
 
+// function changeColorsStudy() {
+//   studyImg.classList.add("hidden");
+// }
+//
+// function changeColorsMeditate() {
+//   meditateImg.classList.add("hidden");
+// }
+//
+// function changeColorsExercise() {
+//   exerciseImg.classList.add("hidden");
+// }
+
+function changeStudyBox() {
+  if (studyButton.classList.contains("active")) {
+    studyButton.classList.remove("active");
+    studyButton.firstElementChild.src = "assets/study.svg"
+  } else if (meditateButton.classList.contains("active") || (exerciseButton.classList.contains("active"))) {
+      studyButton.classList.remove("active");
+      studyButton.firstElementChild.src = "assets/study.svg"
+} else {
+    studyButton.classList.add("active");
+    studyButton.firstElementChild.src = "assets/study-active.svg";
+  }
+}
+
+function changeMeditateBox() {
+  if (meditateButton.classList.contains("active")) {
+    meditateButton.classList.remove("active");
+    meditateButton.firstElementChild.src = "assets/meditate.svg"
+  } else if ((exerciseButton.classList.contains("active")) || (studyButton.classList.contains("active"))) {
+      meditateButton.classList.remove("active");
+      meditateButton.firstElementChild.src = "assets/meditate.svg"
+  } else {
+    meditateButton.classList.add("active");
+    meditateButton.firstElementChild.src = "assets/meditate-active.svg";
+  }
+}
+
+function changeExerciseBox() {
+  if (exerciseButton.classList.contains("active")) {
+    exerciseButton.classList.remove("active");
+    exerciseButton.firstElementChild.src = "assets/exercise.svg"
+  } else if ((meditateButton.classList.contains("active")) || (studyButton.classList.contains("active"))) {
+      exerciseButton.classList.remove("active");
+      exerciseButton.firstElementChild.src = "assets/exercise.svg"
+    } else {
+    exerciseButton.classList.add("active");
+    exerciseButton.firstElementChild.src = "assets/exercise-active.svg";
+}
 }
