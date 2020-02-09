5f21adb77200e4a0250c6134f2a11e45a501303c
Leah Winters <leah.winters24@gmail.com>
2020-02-09 12:54:58 -0700

    Functionalitly to input fields and generorating timer

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ var studyButton = document.getElementById("study");
 var meditateButton = document.getElementById("meditate");
 var exerciseButton = document.getElementById("exercise");
 var boxArray = [studyButton, meditateButton, exerciseButton];
+var startActBtn = document.getElementById('start-act-btn');
+var activeBtn = document.querySelector('.active');
+var warningPopUp = document.getElementById('warning-pop-up');
+var timerPage = document.getElementById('create-new-activity');
+var timerCircle = document.getElementById('timer-circle');
+var circleColor = null;
 
 minutesInput.addEventListener('keyup', onlyNumbersCheck);
 secondsInput.addEventListener('keyup', onlyNumbersCheck);
@@ -13,10 +19,16 @@ document.addEventListener('click', handleClick);
 function handleClick(event) {
   if (event.target.classList.contains('study')) {
     changeColors(event);
+    circleColor = "green";
   } else if (event.target.classList.contains('meditate')) {
     changeColors(event);
+    circleColor = "purple";
   } else if (event.target.classList.contains('exercise')) {
     changeColors(event);
+    circleColor = "pink";
+  } else if (event.target.classList.contains('start-act-btn')) {
+    checkInputFields(event);
+    generateTimerPage();
   }
 }
 
@@ -42,11 +54,48 @@ function removeActiveState(clickedId) {
 }
 
 function onlyNumbersCheck() {
-  var validChars = "0123456789.";
-  if (minutesInput.value !== validChars) {
-    minutesInput.value = "";
+  var replaceMinValue = minutesInput.value.replace('e', '');
+  var replaceSecValue = secondsInput.value.replace('e', '');
+  minutesInput.value = replaceMinValue;
+  secondsInput.value = replaceSecValue;
+}
+
+function checkInputFields(event) {
+  event.preventDefault();
+  var areBtnsClicked = checkCategoryBtns();
+  if (minutesInput.value == 0 || secondsInput.value == 0 || textInput.value == 0 || areBtnsClicked === false){
+    warningPopUp.innerHTML = `<img src="./assets/warning.svg" class="warning-img" alt="warning img">
+    <p class="warning">Please fill out all information!</p>`;
+  } else if (minutesInput.value > 0 && secondsInput.value > 0 && textInput.value > 0) {
+
   }
-  if (secondsInput.value !== validChars) {
-    secondsInput.value = "";
+}
+
+function checkCategoryBtns() {
+  if (studyButton.classList.contains('active') || meditateButton.classList.contains('active') || exerciseButton.classList.contains('active')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function generateTimerPage() {
+  changeCircleColor()
+  timerPage.innerHTML = `<p class="user-activity" id="user-activity">${textInput.value}</p>
+  <p class="timer" id="timer"><span id="minutes">${minutesInput.value}:</span><span id="seconds">${secondsInput.value}</span></p>
+  <div class="timer-circle-holder" id="timer-circle-holder">
+    <div class="timer-circle" id="timer-circle" role="button">
+      <p class="start-complete" id="start-complete">start</p>
+    </div>
+  </div>`
+}
+
+function changeCircleColor() {
+  if (circleColor === 'green') {
+    timerCircle.classList.add('green-circle');
+  } else if (circleColor === 'purple') {
+    timerCircle.classList.add('purple-circle');
+  } else if (circleColor === 'pink') {
+    timerCircle.classList.add('pink-circle');
   }
 }
