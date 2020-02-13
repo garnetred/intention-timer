b174a00b1d208145cb37d47272a5fee6ffc3891b
December G <dgarnetsmith@gmail.com>
2020-02-12 18:41:05 -0700

    Finish iteration 3 and clean up code

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ var warningPopUp = document.getElementById('warning-pop-up');
 var timerPage = document.querySelector('.create-new-activity');
 var currentActivityHeader = document.querySelector('.new-activity')
 var circleColor = null;
+var categoryName = null;
+var categoryColor = null;
 
 minutesInput.addEventListener('keyup', onlyNumbersCheck);
 secondsInput.addEventListener('keyup', onlyNumbersCheck);
@@ -20,16 +22,16 @@ function handleClick(event) {
   event.preventDefault();
   if (event.target.classList.contains('study')) {
     changeColors(event);
-    return circleColor = 'green';
-    console.log(`${circleColor}`);
+    circleColor = 'green';
+    categoryName = 'Study';
   } else if (event.target.classList.contains('meditate')) {
     changeColors(event);
-    return circleColor = 'purple';
-    console.log(`${circleColor}`)
+    circleColor = 'purple';
+    categoryName = 'Meditate';
   } else if (event.target.classList.contains('exercise')) {
     changeColors(event);
-    return circleColor = 'red';
-    console.log(`${circleColor}`)
+    circleColor = 'red';
+    categoryName = 'Exercise';
   } else if (event.target.classList.contains('start-act-btn')) {
     checkInputFields(event);
     generateTimerPage();
@@ -39,8 +41,11 @@ function handleClick(event) {
     countdownTimer(minInt, secInt);
   } else if (event.target.classList.contains('log-activity-btn')) {
     logActivity();
+  } else if (event.target.classList.contains('create-new-activity-btn')) {
+    returnToHomePage(event);
   }
 }
+
 
 function changeColors(event) {
   var clickedId = event.target.id;
@@ -73,7 +78,7 @@ function onlyNumbersCheck() {
 function checkInputFields(event) {
   event.preventDefault();
   var areBtnsClicked = checkCategoryBtns();
-  if (!minutesInput.value || !secondsInput.value|| !textInput.value || areBtnsClicked === false){
+  if (!minutesInput.value || !secondsInput.value || !textInput.value || areBtnsClicked === false){
     warningPopUp.innerHTML = `<img src="./assets/warning.svg" class="warning-img" alt="warning img">
     <p class="warning">Please fill out all information!</p>`;
   } else if (minutesInput.value > 0 && secondsInput.value > 0 && textInput.value > 0) {
@@ -119,10 +124,13 @@ function changeCircleColor() {
   var timerCircle = document.querySelector('#timer-circle');
   if (circleColor === 'green') {
     timerCircle.classList.add('green-circle');
+    categoryColor = 'green-stripe';
   } else if (circleColor === 'purple') {
     timerCircle.classList.add('purple-circle');
+    categoryColor = 'purple-stripe';
   } else if (circleColor === 'red') {
     timerCircle.classList.add('red-circle');
+    categoryColor = 'red-stripe';
   }
 }
 
@@ -148,10 +156,48 @@ function completedTimer() {
 }
 
 function logActivity() {
+  var minInt = parseInt(minutesInput.value);
+  var secInt = parseInt(secondsInput.value);
   var pastActivitiesLog = document.querySelector('.past-activities-log');
-  pastActivitiesLog.innerHTML = `<section class="activity-log-card"><div class = "activity-card-text"><div class=log-card-border><h1 class="card-heading">Meditate</h1>
-  <p class="time-card">Time</p></div><p class="time-card">Activity Name</p></div></section>`
+  pastActivitiesLog.innerHTML = `<section class="activity-log-card"><div class = "activity-card-text"><div class=log-card-border id=${categoryColor}><h1 class="card-heading">${categoryName}</h1>
+  <p class="time-card">${minInt} min ${secInt} seconds</p></div><p class="activity-name-card">${textInput.value}</p></div></section>`
   timerPage.innerHTML = `<button class="create-new-activity-btn">Create A New Activity</button>`;
   currentActivityHeader.innerText = "Completed Activity";
+}
 
+function returnToHomePage() {
+  var homePage = document.getElementsByTagName('main')[0];
+  homePage.innerHTML = `<h2 class="new-activity">New Activity</h2>
+        <section class="create-new-activity card-shadow">
+          <p>Select a category:</p>
+          <section class="category-btn">
+            <button id = "study" class="study focus" type = "button" >
+              <img class="study-img" src="./assets/study.svg" alt="study-img">
+              <p class="labels study-label">Study</p>
+            </button>
+            <button id = "meditate" class="meditate" type = "button" >
+              <img class="meditate-img" src="./assets/meditate.svg" alt="meditate-img">
+              <p class="labels meditate-label">Meditate</p>
+            </button>
+            <button id = "exercise" class="exercise" type = "button" >
+              <img class="exercise-img" src="./assets/exercise.svg" alt="exercise-img">
+              <p class="labels exercise-label">Exercise</p>
+            </button>
+          </section>
+          <p class="what-to-accomplish">What would you like to accomplish during this time?</p>
+          <input id="text-input" type="text" class="accomplish-input">
+          <div class="all-time-input">
+            <div class="time-input minutes">
+              <p class="time-name">Minutes</p>
+              <input id="minutes-input" class="input-time" type="number" name="minutes" min = 0 placeholder="0">
+            </div>
+            <div class="time-input seconds">
+              <p class="time-name">Seconds</p>
+              <input id="seconds-input" class="input-time" type="number" name="seconds" min = 0 placeholder="0">
+            </div>
+          </div>
+          <div class="warning-pop-up">
+          </div>
+          <button type="button" class="start-act-btn"name="Start Activity">Start Activity</button>
+        </section>`
 }
